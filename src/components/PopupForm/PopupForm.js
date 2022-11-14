be9fb96d1872f564b7f8e30e4671635f92d192a5
SPURGE <apple@Apples-MacBook-Pro.local>
2022-11-14 19:37:35 +0530

    changes

--- src/components/PopupForm/PopupForm.js
+++ src/components/PopupForm/PopupForm.js
@@ -1,7 +1,24 @@
-import React from 'react'
+import React from "react";
+import Drawer from "@mui/material/Drawer";
+import { Card, CardActionArea, CardMedia } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+import Address from "../../Pages/Address/Address";
+import Form from "../Form/form";
 
 export default function PopupForm() {
   return (
-    <div>PopupForm</div>
-  )
+    <div>
+      <Drawer anchor={"right"} open={isOpen}>
+        <div>
+          <CloseIcon onClick={openHandler} style={{margin:"1em"}}/>
+        </div>
+        <Card
+          sx={{ width: 500, height: "100%" }}
+          className="ActionAreaCard-cntr"
+        >
+          <Form/>
+        </Card>
+      </Drawer>
+    </div>
+  );
 }
